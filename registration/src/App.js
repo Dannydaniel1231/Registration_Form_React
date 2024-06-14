bfe62bb682d745d1dc0e11b835b77bacbb4fafca
Daniel NIRINGIYIMANA <104560031+Dannydaniel1231@users.noreply.github.com>
2024-06-14 15:26:20 +0300

    Add registration form files

---
+++ registration/src/App.js
@@ -0,0 +1,12 @@
+
+import LoginRegistration from './components/Loginregistraction/LoginRegistration';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginRegistration/>
+    </div>
+  );
+}
+
+export default App;
