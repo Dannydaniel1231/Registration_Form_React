86a3bfcd563d5decb5c6e4ac4d7542846d712da7
Daniel NIRINGIYIMANA <104560031+Dannydaniel1231@users.noreply.github.com>
2024-06-16 20:17:41 +0300

    Your commit message

--- registration/src/components/Loginregistraction/LoginRegistration.jsx
+++ registration/src/components/Loginregistraction/LoginRegistration.jsx
@@ -12,11 +12,37 @@ const LoginRegistration = () => {
           <h1>Login</h1>
           <div className="input-box">
             <input type="text" placeholder="Username" required />
-            <FaUser className='icon'/>
+            <FaUser className="icon" />
           </div>
           <div className="input-box">
             <input type="password" placeholder="password" required />
-            <FaLock  className='icon'/>
+            <FaLock className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>
+              Don't have an account?<a href="#">Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
+      <div className="form-box-login">
+        <form action="">
+          <h1>Registration</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="password" required />
+            <FaLock className="icon" />
           </div>
           <div className="remember-forgot">
             <label>
